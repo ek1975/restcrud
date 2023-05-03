b72164de1e92757744711cd28ff87101a2ceb572
ajoseph <ajoseph@ilsos.gov>
2023-05-03 15:02:15 -0500

    Initial Commit 3.103 - @PathVariable

---
+++ restdemo/src/main/java/test/restcrud/restdemo/rest/DemoRestController.java
@@ -0,0 +1,37 @@
+package test.restcrud.restdemo.rest;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import test.restcrud.restdemo.entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class DemoRestController {
+
+    private List<Student> studentList;
+
+    @PostConstruct
+    private void loadStudents() {
+        studentList = new ArrayList<Student>();
+        studentList.add(new Student("John", "Public"));
+        studentList.add(new Student("Jane", "Public"));
+        studentList.add(new Student("Tim", "Public"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> listAllStudents() {
+        return studentList;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        return studentList.get(studentId);
+    }
+}
